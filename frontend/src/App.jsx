1721f946c610e9c72e57e230497f0859008550ac
Ishita Gupta <igupta@gitam.in>
2026-09-01 14:33:52 +0530

    changed the working and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import BuyerStorefront from './pages/BuyerStorefront';
 import MerchantPage from './pages/MerchantPage';
+import ShoppingBagPage from './pages/ShoppingBagPage';
+import WishlistPage from './pages/WishlistPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<BuyerStorefront />} />
+      <Route path="/bag" element={<ShoppingBagPage />} />
+      <Route path="/wishlist" element={<WishlistPage />} />
       <Route path="/merchant" element={<MerchantPage />} />
     </Routes>
   );
